8c2b480231d8623aff763f787dea10ab516c08f5
GuillhermeHenrique <piresguilherme003@gmail.com>
2025-08-25 17:03:58 -0300

    feat: finalize css

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
+// elements
+const navbar = document.querySelector("#navbar");
+
 const projectsContainer = document.querySelector(".projects");
 const left = document.getElementById("left");
 const right = document.getElementById("right");
 
-const navbar = document.querySelector("#navbar");
+// events
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.style.backgroundColor = "#000000eb";
+  } else {
+    navbar.style.backgroundColor = "transparent";
+  }
+});
 
 right.addEventListener("click", () => {
   projectsContainer.scrollLeft += window.innerWidth;
@@ -10,12 +20,4 @@ right.addEventListener("click", () => {
 
 left.addEventListener("click", () => {
   projectsContainer.scrollLeft -= window.innerWidth;
-});
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
-    navbar.style.backgroundColor = "#000000eb";
-  } else {
-    navbar.style.backgroundColor = "transparent";
-  }
 });
